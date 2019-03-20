117d633a24120037a3148789ab5436f5ecf4c9bc
Rhytah <ritanamono@gmail.com>
2019-03-20 22:04:13 +0300

    reverse a string of choice

---
+++ src/reverse_string.py
@@ -0,0 +1,9 @@
+print("------------------MIRROR MIRROR__MY WORDS------------------")
+
+print("------------------Mirror a word or sentence of choice------------------")
+
+word = str(input("Enter a word or sentence: "))
+new_word=word[::-1]
+
+print ("MIRROR MIRROR__MY WORDS returns")
+print(f"{word}   ",new_word)
